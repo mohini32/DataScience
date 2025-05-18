020a4aecc737442ca66949c2b77ac18d5911e62a
mohini32 <94669778+mohini32@users.noreply.github.com>
2025-05-18 19:46:31 +0530

    Add files via upload

---
+++ 02-SQL/easy_SQL/customerwhoneverorder.sql
@@ -0,0 +1,32 @@
+-- 3.Customers Who Never Order
+-- Table: Customers
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table indicates the ID and name of a customer.
+ 
+
+-- Table: Orders
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | id          | int  |
+-- | customerId  | int  |
+-- +-------------+------+
+-- id is the primary key (column with unique values) for this table.
+-- customerId is a foreign key (reference columns) of the ID from the Customers table.
+-- Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+ 
+
+-- Write a solution to find all customers who never order anything.
+
+-- Return the result table in any order.
+
+
+select c.name as Customers from Customers c left join Orders o on c.id=o.customerId where o.id IS NULL; 
